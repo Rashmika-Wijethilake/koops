0b22e932ac5fb5c0220500b3b7ef7c1a2e46018a
Santhu9600 <santhushi2014@gmail.com>
2023-04-22 13:13:38 +0530

    Made changes to the Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -20,7 +20,7 @@ export const Login = () => {
     return(
         
         <div className="form-container">
-            <div>
+            <div className="Header">
             <HeaderWithoutBtns/>
             </div>
 
