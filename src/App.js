06568bb7c2c94721b1d6268bb24f903c2093da31
Rashmika Wijethilake <bg.rashmikawijethilaka99@gmail.com>
2023-04-22 11:55:53 +0530

    i have created my taska

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Login } from './Components/Login/Login';
+import { Signup } from './Components/Signup/Signup';
+import { useState } from 'react';
+
+
+function App() {
+ 
+   return (
+    <div className="App">
+   <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import bot from '../Assests/chatbot.png';
+import './login.css';
+
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(email);
+    }
+    
+    return(
+        
+        <diV className="form-container">
+            
+            <table>
+                <tr>
+                    <td className="tdone">
+                        <form className="login-form" onSubmit={handleSubmit}>
+                            <label htmlFor="email">Email Address</label>
+                            <input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                            <label htmlFor="Password">Password</label>
+                            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                            <p><button className="Btnl" type="submit">Login</button></p>
+
+                            <div className="fdivl">
+                            <a href="" className="linktwo" >Forgot Password?</a>
+                            <button className="txt">Create new Account.</button><a href="" className="linktwo">Signup</a>
+                            </div>
+                            
+                        </form>
+                    </td>
+                    <td className="tdtwo">
+                    <img className="bot" src={bot}></img>
+                    </td>
+                </tr>
+            </table>
+        </diV>
+    )
+}
+
+
